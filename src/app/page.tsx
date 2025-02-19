ad9d0dee3936aa12674dfeead536d61b6a2fd9ec
SeungMin Lee <wooju2602@gmail.com>
2025-02-20 01:13:41 +0900

    ✨ Feat: 초기전체 레이아웃 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>home</div>;
+import KanbanMetrics from '@/components/dashboard/KanbanMetrics';
+import KanbanMain from '@/components/todo/KanbanMain';
+
+export default function KanbanPage() {
+  return (
+    <div className="min-h-screen select-none bg-gray-900 p-6">
+      <KanbanMetrics />
+      <KanbanMain />
+    </div>
+  );
 }
