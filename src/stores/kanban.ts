71c01b5492273477412ed01e41f925a48fa8081e
SeungMin Lee <wooju2602@gmail.com>
2025-02-20 17:57:30 +0900

    ✨ Feat: 보드 수정 삭제 기능 구현 및 LocalStrage와 연동 기능 구현

--- src/stores/kanban.ts
+++ src/stores/kanban.ts
@@ -1,23 +1,46 @@
-import { create } from 'zustand';
 import { KanbanBoard } from '@/types/kanban';
 import { v4 as uuid } from 'uuid';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface KanbanStore {
   boards: KanbanBoard[];
   addBoard: (title: string) => void;
+  updateBoard: (boardId: string, title: string) => void;
+  deleteBoard: (boardId: string) => void;
 }
 
-export const useKanbanStore = create<KanbanStore>((set) => ({
-  boards: [],
-  addBoard: (title) =>
-    set((state) => ({
-      boards: [
-        ...state.boards,
-        {
-          boardId: uuid(),
-          title,
-          items: [],
-        },
-      ],
-    })),
-}));
+export const useKanbanStore = create<KanbanStore>()(
+  persist(
+    (set) => ({
+      boards: [],
+
+      addBoard: (title) =>
+        set((state) => ({
+          boards: [
+            ...state.boards,
+            {
+              boardId: uuid(),
+              title,
+              items: [],
+            },
+          ],
+        })),
+
+      updateBoard: (boardId, title) =>
+        set((state) => ({
+          boards: state.boards.map((board) =>
+            board.boardId === boardId ? { ...board, title } : board
+          ),
+        })),
+
+      deleteBoard: (boardId) =>
+        set((state) => ({
+          boards: state.boards.filter((board) => board.boardId !== boardId),
+        })),
+    }),
+    {
+      name: 'kanban-storage',
+    }
+  )
+);
